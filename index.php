df4c8ad94a844f08a22c71b9fe5a2ed508db5331
Maligno <guillermoivansalgadosandoval@gmail.com>
2024-07-24 14:03:28 -0500

    mmm

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="icon" href="https://res.cloudinary.com/dvdhtdzwp/image/upload/v1721276122/logoico.jpg">
+    <link rel="icon" href="https://res.cloudinary.com/dvdhtdzwp/image/upload/v1721701771/logoicorm.png">
     <link rel="stylesheet" href="src/css/style-home.css">
     <link rel="stylesheet" href="src/css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
